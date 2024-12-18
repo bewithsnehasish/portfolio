4588a73544ee7ed1b125c7453cf5d0592a8ac5b7
Snehasish Mandal <mandalsneh97@gmail.com>
2024-12-18 17:02:55 +0530

    Updated the  portfolio

---
+++ app/_lib/constants.ts
@@ -0,0 +1,168 @@
+export const techCardsItems = [
+  {
+    name: "Figma",
+    description: "Design Tool",
+    imageUrl: "/imgs/logos/figma-logo.svg",
+    bgColor: "bg-[#0ACF83]/20",
+  },
+  {
+    name: "TypeScript",
+    description: "JavaScript but better",
+    imageUrl: "/imgs/logos/typescript.svg",
+    bgColor: "bg-[#3178C6]/20",
+  },
+  {
+    name: "React",
+    description: "JavaScript Library",
+    imageUrl: "/imgs/logos/react.svg",
+    bgColor: "bg-[#61DAFB]/20",
+  },
+  {
+    name: "NextJS",
+    description: "React framework",
+    imageUrl: "/imgs/logos/nextjs.svg",
+    bgColor: "bg-dark-200/10 dark:bg-white/10",
+  },
+  {
+    name: "Tailwind",
+    description: "CSS framework",
+    imageUrl: "/imgs/logos/tailwind.svg",
+    bgColor: "bg-[#0EA5E9]/20",
+  },
+  {
+    name: "Git",
+    description: "Version control",
+    imageUrl: "/imgs/logos/git.svg",
+    bgColor: "bg-[#F1502F]/20",
+  },
+  {
+    name: "Supabase",
+    description: "Backend tool",
+    imageUrl: "/imgs/logos/supabase.svg",
+    bgColor: "bg-[#3ECF8E]/20",
+  },
+  {
+    name: "Appwrite",
+    description: "Backend tool",
+    imageUrl: "/imgs/logos/appwrite.svg",
+    bgColor: "bg-[#F02D65]/20",
+  },
+];
+
+export const portfolioProjects = [
+  {
+    id: "EmergingIndia",
+    heading: "Emerging India",
+    subheading: "A Full-Fledged NGO Management System",
+    description:
+      "A modern NGO website featuring user-friendly functionalities such as member profile dashboards, lazy loading for faster load times, QR code support for quick link access, and automated certificate generation. Developed with React and TypeScript for the frontend, leveraging Supabase as a robust backend solution.",
+    imageUrl: "/imgs/projects/EmergingIndia.png",
+    techStack: [
+      "TypeScript",
+      "React",
+      "Redux",
+      "Supabase",
+      "TailwindCSS",
+      "shadcn/ui",
+    ],
+    liveDemoUrl: "https://emergingindia.org",
+  },
+  {
+    id: "PaddleLift",
+    heading: "PaddleLift",
+    subheading: "Solutions for Startups and Enterprises",
+    description:
+      "A platform designed to streamline operations for startups and enterprises. Key features include a markdown-to-text preview tool for job descriptions, local storage for secure and persistent user data, and a seamless user interface. The platform also achieved a perfect SEO score of 100 through best coding practices.",
+    imageUrl: "/imgs/projects/PaddleLift.png",
+    techStack: [
+      "NextJS",
+      "React",
+      "ShadcnUI",
+      "TailwindCSS",
+      "SEO Optimization",
+    ],
+    liveDemoUrl: "https://paddlelift.com",
+  },
+  {
+    id: "ManavtaNursery",
+    heading: "Manavta Nursery",
+    subheading: "A Marketplace Platform",
+    description:
+      "Developed a feature-rich marketplace platform that includes task management capabilities. Users can create, save, and delete tasks while enjoying an intuitive interface. Optimized performance through caching and local storage for faster reloads and enhanced usability.",
+    imageUrl: "/imgs/projects/ManavtaNursery.png",
+    techStack: [
+      "ReactJS",
+      "RESTful APIs",
+      "TailwindCSS",
+      "ShadcnUI",
+      "LocalStorage",
+    ],
+    liveDemoUrl: "https://manavtanursery.org",
+  },
+];
+
+export const tooltipItems = [
+  {
+    id: 1,
+    name: "Bogdan Aleksic",
+    designation: "Great job!",
+    image: "/imgs/avatars/bogdan.svg",
+  },
+  {
+    id: 2,
+    name: "Djordje Djurovic",
+    designation: "Incredibly responsible",
+    image: "/imgs/avatars/djordje.jpeg",
+  },
+  {
+    id: 3,
+    name: "Anthony Sean",
+    designation: "Highly recommended!",
+    image: "/imgs/avatars/Anthony.jpg",
+  },
+  {
+    id: 4,
+    name: "Alex Finn",
+    designation: "Enjoyable experience.",
+    image: "/imgs/avatars/Alex.jpg",
+  },
+];
+
+export const testimonialItems = [
+  {
+    id: 1,
+    stars: 5,
+    name: "Alex Finn",
+    proffesion: "Lead Web Designer at knots.io",
+    description:
+      "“Working with Andrija was a very smooth experience from start to finish.  He not only brings strong technical skills to front-end development, but his attention to detail and commitment to delivering pixel-perfect  designs made a huge difference in our project. He is enthusiastic in  suggesting improvements. Always ready to answer questions or provide  updates. If you're looking for a reliable developer who brings  creativity and efficiency to the table, Andrija is the right person!”",
+    image: "/imgs/avatars/Alex.jpg",
+  },
+  {
+    id: 2,
+    stars: 5,
+    name: "Djordje Djurovic",
+    proffesion: "CEO at DigitalityLab",
+    description:
+      "“Andrija is an outstanding frontend specialist and presents consistently high level of work. During our work together, he has demonstrated extraordinary skills in simplifying complex ideas into clean, functional, and visually appealing designs. His communication is to the point and time management is perfect, even for the most pressing deadlines. I would highly recommend him to any company looking to bring on a frontend developer with strong technical skills and an keen eye for design.”",
+    image: "/imgs/avatars/djordje.jpeg",
+  },
+  {
+    id: 3,
+    stars: 5,
+    name: "Anthony Sean",
+    proffesion: "Chief Technology Officer at DealJet",
+    description:
+      "“On several occasions, I have had the privilege to work with Andrija, and he has never failed to impress. While he possesses a great level of knowledge in frontend development, what I find most remarkable about him is his obsession with getting every detail right in order to achieve perfection in the end-user experience. He always pays attention to the project requirements, raises up some pertinent issues, and most importantly delivers beyond the call of duty. Andrija is the kind of person who adds value to the team he's in!”",
+    image: "/imgs/avatars/Anthony.jpg",
+  },
+  {
+    id: 4,
+    stars: 5,
+    name: "Bogdan Aleksic",
+    proffesion: "Investment Educator and Crypto Expert",
+    description:
+      "“I got on board Andrija from Upwork to help me develop a website for my course on blockchain, cryptocurrency, and investment techniques. Because of his effort, I was able to generate considerable sales from my course after launching it, and I have received good reviews about the design and functionality of the website. Andrija is someone I would highly recommend!”",
+    image: "/imgs/avatars/bogdan.svg",
+  },
+];
